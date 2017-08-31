ab5b164edfa7b3b74f59c1a5b65f05719d739ef8
Fabiano Franz <ffranz@redhat.com>
2017-08-31 02:23:27 -0300

    more code reorg

--- gulpfile.js
+++ gulpfile.js
@@ -6,19 +6,27 @@ var sassPaths = [
   'bower_components/foundation-sites/scss'
 ];
 
-gulp.task('sass', function() {
-  return gulp.src('src/scss/app.scss')
+gulp.task('build', ['scss', 'media']);
+
+gulp.task('scss', function() {
+  return gulp.src('src/scss/**/*.scss')
     .pipe($.sass({
       includePaths: sassPaths,
-      outputStyle: 'compressed' // if css compressed **file size**
+      outputStyle: 'compressed'
     })
       .on('error', $.sass.logError))
     .pipe($.autoprefixer({
       browsers: ['last 2 versions', 'ie >= 9']
     }))
-    .pipe(gulp.dest('src/static/css'));
+    .pipe(gulp.dest('public/css'));
 });
 
-gulp.task('default', ['sass'], function() {
-  gulp.watch(['src/scss/**/*.scss'], ['sass']);
+gulp.task('media', function() {
+  return gulp.src('media/**/*')
+    .pipe(gulp.dest('public/media'));
+});
+
+gulp.task('default', ['build'], function() {
+  gulp.watch(['src/scss/**/*.scss'], ['scss']);
+  gulp.watch(['media/**/*'], ['media']);
 });
